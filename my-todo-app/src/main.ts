5dc78d6d65321fe5ef8dcec56471a905584863e6
22214139 <22214139@student.ciu.edu.tr>
2026-03-05 21:59:27 +0200

    feat: display project ID fetched from API

--- my-todo-app/src/main.ts
+++ my-todo-app/src/main.ts
@@ -20,12 +20,13 @@ if (app) {
     app.innerHTML = `
     <div class="card">
       <h1>TS Dashboard</h1>
-      <p>👤 User: <strong>${MyNewUser.name}</strong></p>
+      <p>👤 User: <strong>${MyNewUser.name} </strong></p>
       
       <div class="project-info">
         <h3>Current Project:</h3>
-        <p id="project-title">${realProjectBox.content.title}</p>
-        <p>Status: <span id="status-tag" style="padding: 2px 8px; border-radius: 4px; color: white; background-color: #646cff;">
+        <p><strong>Project ID:</strong> #<span id="project-id">0</span></p>
+<p id="project-title">${realProjectBox.content.title}</p>
+                <p>Status: <span id="status-tag" style="padding: 2px 8px; border-radius: 4px; color: white; background-color: #646cff;">
             ${realProjectBox.content.status}
         </span></p>
         
@@ -47,35 +48,26 @@ if (app) {
 async function fetchRemoteProject() {
     try {
         const titleElement = document.querySelector('#project-title');
+        const idElement = document.querySelector('#project-id'); 
         const statusTag = document.querySelector('#status-tag') as HTMLElement;
 
         if (titleElement) titleElement.textContent = "Loading...";
 
-        
         const randomId = Math.floor(Math.random() * 200) + 1;
-        
-       
         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${randomId}`);
-        
-        if (!response.ok) throw new Error("Network error!");
-        
         const data = await response.json();
 
-       
-        if (titleElement && statusTag) {
+        if (titleElement && idElement && statusTag) {
+            
+            idElement.textContent = data.id.toString(); 
             titleElement.textContent = data.title;
             
-       
+            
             const newStatus = data.completed ? "done" : "in-progress";
             statusTag.textContent = newStatus;
-            
-            
             statusTag.style.backgroundColor = newStatus === "done" ? "#4CAF50" : "#FFA500";
         }
-
     } catch (error) {
-        console.error("خطا در دریافت اطلاعات:", error);
-        const titleElement = document.querySelector('#project-title');
-        if (titleElement) titleElement.textContent = "Failed to load project. Try again!";
+        console.error("Error:", error);
     }
 }
